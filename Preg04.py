0bbdeef9395c0f6ad558c4ec8cf5563126278d34
Victor Manuel <victor-quispe99@hotmail.com>
2020-07-09 21:16:45 -0500

    Pregunta 04-Listo y desarrolladores registrados por Quispe Quispe Victor Manuel

--- Preg04.py
+++ Preg04.py
@@ -1,4 +1,14 @@
 #Autor Quispe Quispe Victor Manuel
+
+
+def integrantes():
+    archivo = open("noticia.txt","at")
+    contenido = "\nCABALLERO MORENO, ISABELLA \nIZQUIERDO ROJAS, LUIS ANGEL \nNAVARRO ESPINOZA, PILAR \nQUISPE QUISPE, VICTOR"
+    archivo.write(contenido)
+    archivo.close()
+
+integrantes()
+
 numero_ingresado = (input("Numero: "))
 revertir = 0
 try:
