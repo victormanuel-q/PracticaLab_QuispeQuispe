257382b657365642622e12199d0249978108a932
Isabella Caballero <susanamoreno9498@gmail.com>
2020-07-09 19:41:03 -0500

    Pregunta 02-Listo

--- Preg02.py
+++ Preg02.py
@@ -1,1 +1,10 @@
 #Autor: Isabella Caballero Moreno
+
+def Preg02():
+    correo=input("Validar: ")
+    if '@untels.edu.pe' in correo:
+        print("Si es correo de la UNTELS")
+    else:
+        print("No es correo de la UNTELS")
+
+Preg02()
