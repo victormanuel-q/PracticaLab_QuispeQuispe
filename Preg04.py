430dd144887beac95d114149605d197ae83fb5e6
Victor Manuel <victor-quispe99@hotmail.com>
2020-07-09 20:43:34 -0500

    Pregunta 04-listo y desarrolladores registrados por Quispe Quispe Victor Manuel

--- Preg04.py
+++ Preg04.py
@@ -1,1 +1,12 @@
 #Autor Quispe Quispe Victor Manuel
+numero_ingresado = (input("Numero: "))
+revertir = 0
+try:
+    valor = int(numero_ingresado)
+    while valor > 0:
+        residuo = valor % 10
+        revertir = (revertir * 10) + residuo
+        valor //= 10
+    print('Resultado: ', revertir)
+except ValueError:
+    print("Ese numero no es valido.!")
