fc1c358efe8927d1d53c1dfe809e71f23bc2df8f
Luis Angel Izquierdo <luisangel19992412@gmail.com>
2020-07-09 19:10:53 -0500

    Pregunta 01 - Listo

--- Preg01.py
+++ Preg01.py
@@ -1,1 +1,16 @@
 #AUTOR: LUIS IZQUIERDO
+
+def pregunta1(numero):
+    cont = 0
+    for n in range(1, numero+1):
+        if numero % n == 0:
+            cont += 1
+
+    if cont == 2:
+        print("El numero ingresado si es primo")
+    else:
+        print("El numero ingresado no es primo")
+        
+numero = int(input("Dato de entrada: "))
+print()
+pregunta1(numero)
